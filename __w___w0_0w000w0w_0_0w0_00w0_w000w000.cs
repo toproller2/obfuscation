e5c1f99f58b54fafce346ed2566f3f1f1ce9800e
toproller2 <cursor171@gmail.com>
2025-05-24 17:04:51 +0300

    Add files via upload

---
+++ __w___w0_0w000w0w_0_0w0_00w0_w000w000.cs
@@ -0,0 +1,160 @@
+
+// -- --- .-. ... .  -.-. .-.. .- ... ...
+// __w___w0_0w000w0w_0_0w0_00w0_w000w000
+// .... ..- -- .- -.
+
+/// <summary>
+/// when naming a method, we put the letter "b" at the beginning, that is, it means the beginning.
+/// We replace the Morse code characters "." and "-" with "0" and "_", respectively.
+/// When translating Morse code, you should get an English name for a method or variable or other named things.
+/// To determine the letter after writing it, we put a w, which means a space, so we denote that the next letter begins.
+/// example:
+/// .--. .-. .. -. - -- . ... ... .- --. .
+/// this encryption will be equal to:
+/// b0__0w0_0w00w_0w_w__w0w000w000w0_w__0w0
+/// if translate to normal method name "PrintMessage"
+/// </summary>
+public class b0000w00_w__w0_w_0 : 
+    I_00w00w000w0__0w0_00w0_w_0__w00w_0w00_0w___
+{
+    private string _0w0_w__0; // -. .- -- .
+    private int b0_w__0w0; // .- --. .
+    private int __w___w_0w0w_0__; // -- --- -. . -.--
+    private float b0000w0w00w__0w0000w_; // .... . .. --. .... -
+
+    private int b0000w0w0_w0_00w_w0000;   // .... . .- .-.. - ....
+
+    private string
+        b0_00w0_w000w_w0_0w0w__w0w__w_000w0w0_0w000w0w_0w_w0_w_0w_0_0w0; // .-.. .- ... -  .-. . -- . -- -... . .-.  ... . -. - .- -. -.-. .
+
+    private b0000w00_w__w0_w_0
+        _w0_w0_0w__0w0w_w_w___w0__0w00_w_0W_0_0w0000;  // - .- .-. --. . - - --- .--. ..- -. -.-. ....
+
+    public b0000w00_w__w0_w_0(string c_0w0_w__0, int cb0_w__0w0,
+        int c__w___w_0w0w_0__, float cb0000w0w00w__0w0000w_,
+        int cb0000w0w0_w0_00w_w0000)
+    {
+        _0w0_w__0 = c_0w0_w__0;
+        b0_w__0w0 = cb0_w__0w0;
+        __w___w_0w0w_0__ = c__w___w_0w0w_0__;
+        b0000w0w00w__0w0000w_ = cb0000w0w00w__0w0000w_;
+        b0000w0w0_w0_00w_w0000 = cb0000w0w0_w0_00w_w0000;
+    }
+
+    public string p_0w0_w__0 { get => _0w0_w__0; }
+
+    public int pb0w__0w0
+    {
+        get => b0_w__0w0;
+        set => b0_w__0w0 = value;
+    }
+
+    public int p__w___w_0w0w_0__
+    {
+        get => __w___w_0w0w_0__;
+        set => __w___w_0w0w_0__ = value;
+    }
+
+    public float pb0000w0w00w__0w0000w_
+    {
+        get => b0000w0w00w__0w0000w_;
+        set => b0000w0w00w__0w0000w_ = value;
+    }
+
+    public int pb0000w0w0_w0_00w_w0000
+    {
+        get => b0000w0w0_w0_00w_w0000;
+        set => b0000w0w0_w0_00w_w0000 = value;
+    }
+
+    // - .- .-.. -.-
+    // argument: .-. . -.-. . .. ...- . .-
+    // agrument -- . ... ... .- --. .
+    public void _w0_w0_w0_00w_0_(
+        b0000w00_w__w0_w_0 b0_0w0w_0_0w0w00w000_w0w0_0,
+        string __w0w000w000w0_w__)
+    {
+        b0_0w0w_0_0w0w00w000_w0w0_0.
+            b0_00w0_w000w_w0_0w0w__w0w__w_000w0w0_0w000w0w_0w_w0_w_0w_0_0w0
+            = __w0w000w000w0_w__;
+    }
+
+    // ... . - - .- .-. --. . -
+    public void b000w0w_w_w0_w0_0w__w0w_(b0000w00_w__w0_w_0
+        p_w0_w0_0w__0w0w_w_w___w0__0w00_w_0W_0_0w0000)
+    {
+        _w0_w0_0w__0w0w_w_w___w0__0w00_w_0W_0_0w0000 =
+            p_w0_w0_0w__0w0w_w_w___w0__0w00_w_0W_0_0w0000;
+    }
+
+    // .--. ..- -. -.-. ....
+    public void b0__0w00_w_0w_0_0w0000()
+    {
+        if (_w0_w0_0w__0w0w_w_w___w0__0w00_w_0W_0_0w0000 == null)
+        {
+            throw new NullReferenceException();
+        }
+        _w0_w0_0w__0w0w_w_w___w0__0w00_w_0W_0_0w0000.
+            pb0000w0w0_w0_00w_w0000 -= (int)pb0000w0w00w__0w0000w_
+            * b0_w__0w0;
+    }
+
+    // ... .-.. . . .--.
+    // argument: ... .-.. . . .--.  -.. ..- .-. .- - .. --- -. 
+    // -.. ..- .-. .- - .. --- -.
+    public void b000w0_00w0w0w0__0(
+        b000w0_00w0w0w0__0w_00w00_w0_0w0_w_00w___w_0
+        _00w00_w0_0w0_w_w00w___w_0)
+    {
+        int b0000w0w0_w0_00w_w0000w_000w___w_0w00_w000 = 0; // .... . .- .-.. - .... -... --- -. ..- ...
+
+        switch (_00w00_w0_0w0_w_w00w___w_0)
+        {
+            case b000w0_00w0w0w0__0w_00w00_w0_0w0_w_00w___w_0.
+            b0_00w___w0__:
+                b0000w0w0_w0_00w_w0000w_000w___w_0w00_w000 = 1;
+                break;
+
+            case b000w0_00w0w0w0__0w_00w00_w0_0w0_w_00w___w_0.
+            b0w_0w___w00_w__0w0000:
+
+                b0000w0w0_w0_00w_w0000w_000w___w_0w00_w000 = 2;
+                break;
+
+            case b000w0_00w0w0w0__0w_00w00_w0_0w0_w_00w___w_0.
+            b0_00w___w_0__0:
+
+                b0000w0w0_w0_00w_w0000w_000w___w_0w00_w000 = 3;
+                break;
+        }
+
+        pb0000w0w0_w0_00w_w0000 += b0000w0w0_w0_00w_w0000w_000w___w_0w00_w000;
+    }
+
+    // -.. .. ... .--. .-.. .- -.-- .. -. ..-. ---
+    public void _00w00w000w0__0w0_00w0_w_0__w00w_0w00_0w___()
+    {
+        Debug.Log($"_0w0_w__0 = {p_0w0_w__0}\n" +
+            $"b0_w__0w0 = {pb0w__0w0}\n" +
+            $"__w___w_0w0w_0__ = {p__w___w_0w0w_0__}\n" +
+            $"b0000w0w00w__0w0000w_ = {b0000w0w00w__0w0000w_}\n" +
+            $"b0000w0w0_w0_00w_w0000 = {pb0000w0w0_w0_00w_w0000}");
+    }
+}
+
+// ... .-.. . . .--.  -.. ..- .-. .- - .. --- -.
+// value: .-.. --- .--
+// value: . -. --- ..- --. ....
+// value: .-.. --- -. --.
+public enum b000w0_00w0w0w0__0w_00w00_w0_0w0_w_00w___w_0
+{
+    b0_00w___w0__,
+    b0w_0w___w00_w__0w0000,
+    b0_00w___w_0__0
+}
+
+// -.. .. ... .--. .-.. .- -.-- .. -. ..-. ---
+public interface I_00w00w000w0__0w0_00w0_w_0__w00w_0w00_0w___
+{
+    void _00w00w000w0__0w0_00w0_w_0__w00w_0w00_0w___();
+}
