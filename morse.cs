3b86f00351c17bd83f7bb27f2de97ac9c5b85e47
toproller2 <cursor171@gmail.com>
2025-05-26 17:08:43 +0300

    Add files via upload

---
+++ morse.cs
@@ -0,0 +1,20 @@
+public class DPDPwpdwpdp
+{
+    private int _pddpwdddwpddwpwpdp;
+    private string _dpwpdwddwp;
+    
+    public DPDPwpdwpdp(int pddpwdddwpddwpwpdp, string dpwpdwddwp)
+    {
+        _pddpwdddwpddwpwpdp = pddpwdddwpddwpwpdp;
+        _dpwpdwddwp = dpwpdwddwp;
+    }
+
+    public int Pddpwdddwpddwpwpdp { get => _pddpwdddwpddwpwpdp;}
+
+    public string Dpwpdwddwp { get => _dpwpdwddwp;}
+
+    public void PPPwdwpdwpdpwdwPwdpwddpwppwdpwpw()
+    {
+        
+    }
+}
